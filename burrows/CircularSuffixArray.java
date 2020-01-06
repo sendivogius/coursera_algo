03fe558661f2a9a18c5af49524f53fc09a6ce2e0
sendivogius <sendivogius@gmail.com>
2020-01-06 17:46:27 +0100

    100 pts solution

--- burrows/CircularSuffixArray.java
+++ burrows/CircularSuffixArray.java
@@ -3,32 +3,41 @@ import edu.princeton.cs.algs4.StdOut;
 import java.util.Arrays;
 import java.util.Comparator;
 
+
 public class CircularSuffixArray {
     private class StringSuffixComparator implements Comparator<Integer> {
 
         public StringSuffixComparator() {
         }
 
-        public String getSuffix(int offset) {
-            if (offset == 0)
-                return str;
-            return str.substring(offset) + str.substring(0, offset - 1);
+        private char charAt(int index, int offset) {
+            return str.charAt((index + offset) % len);
         }
 
         @Override
         public int compare(Integer index1, Integer index2) {
-            return getSuffix(index1).compareTo(getSuffix(index2));
+            for (int i = 0; i < len; i++) {
+                char left = charAt(index1, i);
+                char right = charAt(index2, i);
+                if (left < right)
+                    return -1;
+                if (left > right)
+                    return 1;
+            }
+            return 0;
         }
     }
 
     private final String str;
+    private final int len;
     private final Integer[] suffixOrder;
 
     public CircularSuffixArray(String s) {
         if (s == null)
             throw new IllegalArgumentException();
         str = s;
-        suffixOrder = new Integer[length()];
+        len = str.length();
+        suffixOrder = new Integer[len];
         for (int i = 0; i < length(); i++) {
             suffixOrder[i] = i;
         }
@@ -42,7 +51,7 @@ public class CircularSuffixArray {
     }
 
     public int length() {
-        return str.length();
+        return len;
     }
 
     public static void main(String[] args) {
